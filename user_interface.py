d027011c854d910b8e79b13cb53be1a3b8f1cb5e
Haoyu Sun <raptorsun@gmail.com>
2019-11-07 23:17:03 +0100

    traffic box

--- user_interface.py
+++ user_interface.py
@@ -2,10 +2,90 @@ import datetime
 import time
 from collections import Counter
 import threading
+import weakref
 
 import npyscreen
 
 UI_REFRESH_INTERVAL = 0.5
+
+
+class OneDecimalSlider(npyscreen.Slider):
+    def translate_value(self):
+        return f'{self.value:.1f} / {self.out_of:.1f}'
+
+
+class TitleOneDecimalSlider(npyscreen.TitleSlider):
+    _entry_type = OneDecimalSlider
+
+
+class TrafficBox(npyscreen.BoxTitle):
+    def set_values(self, val_10s, val_2m, val_lifetime):
+        self._10s_value = val_10s
+        self._2m_value = val_2m
+        self._lifetime_value = val_lifetime
+
+        # automatically increase slider range
+        tmp_max = max(val_10s, val_2m, val_lifetime)
+        if tmp_max == 0:
+            self._slider_max = 50
+        elif self._slider_max < tmp_max:
+            self._slider_max = tmp_max * 1.3
+        elif tmp_max < self._slider_max * 0.6:
+            self._slider_max = self._slider_max * 0.6
+        self._my_widgets[0].entry_widget.out_of = self._slider_max
+        self._my_widgets[1].entry_widget.out_of = self._slider_max
+        self._my_widgets[2].entry_widget.out_of = self._slider_max
+
+        self._my_widgets[0].value = self._10s_value
+        self._my_widgets[1].value = self._2m_value
+        self._my_widgets[2].value = self._lifetime_value
+
+    def make_contained_widget(self, contained_widget_arguments=None):
+        self._my_widgets = []
+        _rely = self.rely+1
+        _relx = self.relx+1
+        width = self.width
+
+        self._slider_max = 10
+        self._10s_value = 0
+        self._2m_value = 0
+        self._lifetime_value = 0
+
+        self._my_widgets.append(TitleOneDecimalSlider(
+            self.parent,
+            rely=_rely,
+            relx=_relx,
+            max_width=width - 4,
+            name='LPS 10s',
+            out_of=self._slider_max,
+            value=self._10s_value,
+            editable=False
+        ))
+        _rely += 1
+        self._my_widgets.append(TitleOneDecimalSlider(
+            self.parent,
+            rely=_rely,
+            relx=_relx,
+            max_width=width - 4,
+            name='LPS 2m',
+            out_of=self._slider_max,
+            value=self._2m_value,
+            editable=False
+        ))
+        _rely += 1
+        self._my_widgets.append(TitleOneDecimalSlider(
+            self.parent,
+            rely=_rely,
+            relx=_relx,
+            max_width=width - 4,
+            name='LPS Lifetime',
+            out_of=self._slider_max,
+            value=self._lifetime_value,
+            editable=False
+        ))
+        _rely += 1
+
+        self.entry_widget = weakref.proxy(self._my_widgets[0])
 
 
 class Dashboard(npyscreen.Form):
@@ -17,55 +97,46 @@ class Dashboard(npyscreen.Form):
     def create(self):
         self.keypress_timeout = 10
 
+        # press Escape to quit
         self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE] = self.exit_application
 
         height, width = self.useable_space()
-        max_widget_width = int(0.4 * width)
 
-        self._lps_frame_text = self.add(
-            npyscreen.TitleFixedText, name='LPS 10s', value='0', editable=False,
-            max_width=max_widget_width, max_height=1)
-        self._lps_scene_text = self.add(
-            npyscreen.TitleFixedText, name='LPS 2min', value='0', editable=False,
-            max_width=max_widget_width, max_height=1)
-        self._lps_lifetime_text = self.add(
-            npyscreen.TitleFixedText, name='LPS Lifetime', value='0', editable=False,
-            max_width=max_widget_width, max_height=1)
         self._time_text = self.add(
-            npyscreen.TitleFixedText, name='Time', value='0', editable=False,
-            max_width=max_widget_width, max_height=1)
+            npyscreen.TitleFixedText, name='Time', value='0', editable=False)
 
-        available_height = height - 4 - 4  # margin  = 4px, 4 text boxes = 4px
+        self._lps_box = self.add(
+            TrafficBox, name='Traffic', max_height=10, max_width=width / 2, editable=False)
+        self._lps_box.set_values(val_10s=0, val_2m=0, val_lifetime=0)
+
+        available_height = height - 4 - self._time_text.height - \
+            self._lps_box.height
         self._section_hit_list = self.add(
-            npyscreen.TitlePager, name="Popular Sections", editable=False,
-            max_width=max_widget_width, max_height=int(available_height/2))
+            npyscreen.TitlePager, name="Popular Sections", editable=False, max_height=int(available_height/2))
 
         self._alert_list = self.add(
-            npyscreen.TitleBufferPager, name="Alerts", editable=False,
-            max_width=width - 4, max_height=int(available_height/2))
+            npyscreen.TitleBufferPager, name="Alerts", editable=False, max_height=int(available_height/2))
 
     def afterEditing(self):
         self.parentApp.NEXT_ACTIVE_FORM = None
 
     def while_waiting(self):
-        self._lps_frame_text.value = '{:10.3f}'.format(
-            self._stats.get('lps_frame', 0))
-        self._lps_frame_text.display()
-        self._lps_lifetime_text.value = '{:10.3f}'.format(
-            self._stats.get('lps_lifetime', 0))
-        self._lps_lifetime_text.display()
-        self._lps_scene_text.value = '{:10.3f}'.format(
-            self._stats.get('lps_scene', 0))
-        self._lps_scene_text.display()
+        self._lps_box.set_values(
+            val_10s=self._stats.get('lps_frame', 0),
+            val_2m=self._stats.get('lps_scene', 0),
+            val_lifetime=self._stats.get('lps_lifetime', 0)
+        )
+        self._lps_box.display()
+
         self._time_text.value = time.asctime()
         self._time_text.display()
 
         heatmap = self._stats.get('heat_map_frame', {})
-        top_5_sections = Counter(heatmap).most_common(5)
-        top_5_sections_str = [
-            '{} {}'.format(x[0], x[1]) for x in top_5_sections
+        top_sections = Counter(heatmap).most_common()
+        top_sections_hits_str = [
+            '{} {}'.format(x[0], x[1]) for x in top_sections
         ]
-        self._section_hit_list.values = top_5_sections_str
+        self._section_hit_list.values = top_sections_hits_str
         self._section_hit_list.display()
 
         if self._alert_q and not self._alert_q.empty():
@@ -89,7 +160,7 @@ class MonitorUI(npyscreen.NPSAppManaged):
         self._alert_q = alert_q
 
     def onStart(self):
-        self.addForm('MAIN', Dashboard, name='Dashboard',
+        self.addForm('MAIN', Dashboard, name='HTTP Log Monitor',
                      stats=self._stats_dict, alert_q=self._alert_q)
 
     def signal_exit(self):
